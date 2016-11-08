63779dfe58a7180bf7d413c62413223273f9fc04
Guo Pei <amazingguo@gmail.com>
2016-11-08 15:14:49 -0700

    add test.lua

--- CompactBilinearPooling.lua
+++ CompactBilinearPooling.lua
@@ -20,11 +20,8 @@ function CompactBilinearPooling:reset()
     self.gradInputFlatPermute = {}
 end
 
---[[
-Generate vectors and sketch matrix for tensor count sketch
-This is only done once during graph construction, and fixed during each
-operation
-]]--
+-- generate random vectors h1, h2, s1, s2.
+-- according to "Algorithm 2 Tensor Sketch Projection" step 1.
 function CompactBilinearPooling:sample()
    self.rand_h_1:uniform(0,self.outputSize):ceil()
    self.rand_h_2:uniform(0,self.outputSize):ceil()
@@ -32,16 +29,16 @@ function CompactBilinearPooling:sample()
    self.rand_s_2:uniform(0,2):floor():mul(2):add(-1)
 end
 
---[[
-PSI function as shown in "Algorithm 2 Tensor Sketch Projection"
-in "Compact Bilinear Pooling" paper.
-]]--
+-- generate psi function.
+-- according to "Algorithm 2 Tensor Sketch Projection" step 2.
 function CompactBilinearPooling:psiFunc()
    self.psi:zero()
    self.psi[1]:indexAdd(2,self.rand_h_1,torch.cmul(self.rand_s_1:repeatTensor(self.flatBatchSize,1),self.inputFlatPermute[1]))
    self.psi[2]:indexAdd(2,self.rand_h_2,torch.cmul(self.rand_s_2:repeatTensor(self.flatBatchSize,1),self.inputFlatPermute[2]))
 end
 
+-- compute phi(x).
+-- according to "Algorithm 2 Tensor Sketch Projection" step 3.
 function CompactBilinearPooling:conv(x,y)
    local batchSize = x:size(1)
    local dim = x:size(2)
@@ -65,34 +62,44 @@ function CompactBilinearPooling:conv(x,y)
 end
 
 function CompactBilinearPooling:updateOutput(input)
-    self.input = input
-    local inputSizes_1 = self.input[1]:size()
-    local inputSizes_2 = self.input[2]:size()
-    self.batchSize = inputSizes_1[1]
-    self.height = inputSizes_1[3]
-    self.width = inputSizes_1[4]
-
+    -- check two inputs
+    assert(#input == 2, "expect 2 inputs, get %d instead.".format(#input))
+    -- check 4D inputs
+    for i = 1, #input do
+        assert(input[i]:dim() == 4, "expect 4D input Tensor, get %dD instead for input %d.".format(input[i]:dim(), i))
+    end
+    -- check shape match for dimension 1, 3, 4
+    for i = 1, input[1]:dim() do
+        if i ~= 2 then
+            assert(input[1]:size(i) == input[2]:size(i), 
+                "dimension %d size mismatch, %d vs %d.".format(i, input[1]:size(i), input[2]:size(i)))
+        end
+    end
+    self.batchSize = input[1]:size(1)
+    self.height    = input[1]:size(3)
+    self.width     = input[1]:size(4)
+    -- step 1 in algorithm 2.
     if 0==#self.rand_h_1:size() then
-        self.rand_h_1:resize(inputSizes_1[2])
-        self.rand_h_2:resize(inputSizes_2[2])
-        self.rand_s_1:resize(inputSizes_1[2])
-        self.rand_s_2:resize(inputSizes_2[2])
+        self.rand_h_1:resize(input[1]:size(2))
+        self.rand_h_2:resize(input[2]:size(2))
+        self.rand_s_1:resize(input[1]:size(2))
+        self.rand_s_2:resize(input[2]:size(2))
         self:sample()  -- samples are fixed
     end
-
-    for i = 1, #self.input do
-        local input_permute = self.input[i]:permute(1,3,4,2):contiguous()
+    -- convert the input from 4D to 2D and expose dimension 2 outside. 
+    for i = 1, #input do
+        local input_permute      = input[i]:permute(1,3,4,2):contiguous()
         self.inputFlatPermute[i] = input_permute:view(-1, input_permute:size(input_permute:dim()))
     end
-   
+    -- step 2 in algorithm 2.
     self.flatBatchSize = self.inputFlatPermute[1]:size(1)
     self.psi:resize(2, self.flatBatchSize, self.outputSize)
     self:psiFunc()
-    
+    -- step 3 in algorithm 2.
     local output_flat = self:conv(self.psi[1], self.psi[2])
-    self.output = output_flat:reshape(self.batchSize, self.height, self.width, self.outputSize)
-
-    self.output = self.output:sum(2):sum(3):squeeze()
+    -- reshape output and sum pooling over dimension 2 and 3.
+    self.output       = output_flat:reshape(self.batchSize, self.height, self.width, self.outputSize)
+    self.output       = self.output:sum(2):sum(3):squeeze()
     
     return self.output
 end
@@ -101,7 +108,7 @@ function CompactBilinearPooling:updateGradInput(input, gradOutput)
     local batchSize = self.inputFlatPermute[1]:size(1)
     self.gradInput = self.gradInput or {}
 
-    for k=1, #self.input do
+    for k=1, #input do
         self.gradInputFlatPermute[k] = self.gradInputFlatPermute[k] or self.inputFlatPermute[k].new()
         self.gradInputFlatPermute[k]:resizeAs(self.inputFlatPermute[k]):zero()
         self.convBuf = self.convBuf or gradOutput.new()
@@ -120,9 +127,9 @@ function CompactBilinearPooling:updateGradInput(input, gradOutput)
     end
    
    
-    for i = 1, #self.input do
+    for i = 1, #input do
         local gradInputView = self.gradInputFlatPermute[i]:view(self.batchSize, self.height, self.width, -1)
-        self.gradInput[i] = gradInputView:permute(1,4,2,3):contiguous()
+        self.gradInput[i]   = gradInputView:permute(1,4,2,3):contiguous()
     end
 
     return self.gradInput
---
+++ test/test.lua
@@ -0,0 +1,89 @@
+require 'nn'
+require 'cunn'
+
+local cbptest = torch.TestSuite()
+local precision = 1e-5
+local debug = false
+
+function cbptest.testPsi()
+    local batch   = 32
+    local dim_1   = 25
+    local dim_2   = 25
+    local height  = 16
+    local width   = 16
+    local outSize = 64
+    local iters   = 100
+    local x       = torch.rand(batch,dim_1,height,width):cuda()
+    local y       = torch.rand(batch,dim_2,height,width):cuda()
+    local outSum  = 0
+    
+    local model   = nn.CompactBilinearPooling(outSize):cuda()
+    for i=1,iters do
+        model:forward({x,y})
+        outSum = outSum + model.psi[1][1]:dot(model.psi[2][1])
+    end
+    local xy = x:permute(1,3,4,2):view(-1,dim_1)[1]:dot(y:permute(1,3,4,2):view(-1,dim_2)[1])
+    local diff = math.abs(outSum/iters - xy)
+    assert(diff / xy < .1, 'error_ratio='..diff / xy..', E[<phi(x,h,s),phi(y,h,s)>]=<x,y>')
+end
+
+function cbptest.testConv()
+   local x = torch.CudaTensor{{1,2,3},{2,3,4}}
+   local y = torch.CudaTensor{{1,1,1},{2,2,2}}
+   local c = nn.CompactBilinearPooling(x:size(2))
+   local ans = torch.CudaTensor{{6,6,6},{18,18,18}}
+   local output = torch.CudaTensor()
+   output = c:conv(x,y)  -- cuda only
+   ans:add(-output)
+   assert(ans:norm() < precision, ans:norm())
+
+   local x = torch.CudaTensor{{1,2,3,1,1},{2,3,4,1,1}}
+   local y = torch.CudaTensor{{1,1,1,1,1},{2,2,2,1,1}}
+   local c = nn.CompactBilinearPooling(x:size(2))
+   local ans = torch.CudaTensor{{8,8,8,8,8},{17.5,17.5,17.75,17.75,17.5}}
+   output = c:conv(x,y)
+   ans:add(-output)
+   assert(ans:norm() < precision, ans:norm())
+end
+
+function cbptest.testForward()
+    local batch   = 32
+    local dim_1   = 25
+    local dim_2   = 25
+    local height  = 16
+    local width   = 16
+    local outSize = 64
+    local iters   = 100
+    local x       = torch.rand(batch,dim_1,height,width):cuda()
+    local y       = torch.rand(batch,dim_2,height,width):cuda()
+    local z       = torch.rand(batch,dim_1,height,width):cuda()
+    local w       = torch.rand(batch,dim_2,height,width):cuda()
+    local cbp     = nn.CompactBilinearPooling(outSize):cuda()
+    local part    = nn.ParalleTable():add(nn.Reshape(height*width, true))
+    local bp      = nn.Sequential():add(part):add(nn.MM(false, true)):cuda()
+    
+    # Compact Bilinear Pooling results
+    local cbp_xy = cbp({x, y})
+    local cbp_zw = cbp({z, w})
+
+    # (Original) Bilinear Pooling results
+    local bp_xy = bp({x, y})
+    local bp_zw = bp({z, w})
+    
+    local cbp_kernel = torch.mm(cbp_xy, cbp_zw):sum(2)
+    local bp_kernel  = torch.mm(bp_xy, bp_zw):sum(2)
+    local ratio      = torch.cdiv(cbp_kernel, bp_kernel)
+    print("ratio between Compact Bilinear Pooling (CBP) and Bilinear Pooling (BP):")
+    print(ratio)
+    assert(torch.all(torch.abs(ratio - 1) < precision))
+    print("Passed.")
+    
+end
+
+function cbp.test(tests, _debug)
+   debug = _debug or false
+   mytester = torch.Tester()
+   mytester:add(cbptest)
+   math.randomseed(os.time())
+   mytester:run(tests)
+end

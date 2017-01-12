04ebd949bbf9a9fe63c104214a74b3e85d5176b8
Guo Pei <amazingguo@gmail.com>
2017-01-11 20:21:54 -0700

    revise everything

--- CompactBilinearPooling.lua
+++ CompactBilinearPooling.lua
@@ -1,54 +1,45 @@
-local CompactBilinearPooling, parent = torch.class('nn.CompactBilinearPooling', 'nn.Module')
-
 require 'spectralnet'
 
-function CompactBilinearPooling:__init(outputSize, homo)
-    assert(outputSize and outputSize >= 1, 'missing outputSize!')
-    self.outputSize = outputSize 
-    self.homo = homo or false
-    self:reset()
-end
+local ComBiPooling, parent = torch.class('nn.CompactBilinearPooling', 'nn.Module')
 
-function CompactBilinearPooling:reset()
-    self.rand_h_1 = torch.Tensor()
-    self.rand_h_2 = torch.Tensor()
-    self.rand_s_1 = torch.Tensor()
-    self.rand_s_2 = torch.Tensor()
-    self.psi = torch.Tensor()
-    self.gradInput = {}
-    self.convBuf = torch.Tensor()
-    self.inputFlatPermute = {}
-    self.gradInputFlatPermute = {}
+function ComBiPooling:__init(output_size)
+    assert(output_size and output_size >= 1, 'missing outputSize...')
+    self.output_size = output_size 
+    self.flat_input = {}
+    self.hash_input = torch.Tensor()
 end
 
 -- generate random vectors h1, h2, s1, s2.
 -- according to "Algorithm 2 Tensor Sketch Projection" step 1.
-function CompactBilinearPooling:sample()
-   self.rand_h_1:uniform(0,self.outputSize):ceil()
-   self.rand_h_2:uniform(0,self.outputSize):ceil()
-   self.rand_s_1:uniform(0,2):floor():mul(2):add(-1)
-   self.rand_s_2:uniform(0,2):floor():mul(2):add(-1)
+function ComBiPooling:genRand(size_1, size_2)
+    self.rand_h_1 = torch:Tensor(size_1):uniform(0,self.outputSize):ceil()
+    self.rand_h_2 = torch:Tensor(size_2):uniform(0,self.outputSize):ceil()
+    self.rand_s_1 = torch:Tensor(size_1):uniform(0,2):floor():mul(2):add(-1)
+    self.rand_s_2 = torch:Tensor(size_2):uniform(0,2):floor():mul(2):add(-1)
 end
 
--- generate psi function.
--- according to "Algorithm 2 Tensor Sketch Projection" step 2.
-function CompactBilinearPooling:psiFunc()
-    self.psi:zero()
-    if self.homo then           
-        self.psi[1]:indexAdd(2,self.rand_h_1,
-            torch.cmul(self.rand_s_1:repeatTensor(self.flatBatchSize,1),self.inputFlatPermute[1]))
-        self.psi[2]:indexAdd(2,self.rand_h_1,
-            torch.cmul(self.rand_s_1:repeatTensor(self.flatBatchSize,1), self.inputFlatPermute[2]))
-    else
-        self.psi[1]:indexAdd(2,self.rand_h_1,
-            torch.cmul(self.rand_s_1:repeatTensor(self.flatBatchSize,1),self.inputFlatPermute[1]))
-        self.psi[2]:indexAdd(2,self.rand_h_2,
-            torch.cmul(self.rand_s_2:repeatTensor(self.flatBatchSize,1), self.inputFlatPermute[2]))
+function ComBiPooling:getHashInput()
+    self.hash_input:zero()
+    self.hash_input[1]:indexAdd(2,self.rand_h_1,
+        torch.cmul(self.rand_s_1:repeatTensor(self.flat_size,1),self.flat_input[1]))
+    self.hash_input[2]:indexAdd(2,self.rand_h_2,
+        torch.cmul(self.rand_s_2:repeatTensor(self.flat_size,1), self.flat_input[2]))    
+end
+
+function ComBiPooling:checkInput(input)
+    assert(2 == #input, string.format("expect 2 inputs but get %d...", #input))
+    assert(4 == input[1].nDimensions() and 4 == input[2].nDimensions(), 
+        string.format("wrong input dimensions, required (4, 4), but get (%d, %d)", 
+        input[1].nDimensions(), input[2].nDimensions()))
+    for dim = 1, 4 do
+        if dim ~= 2 then
+            assert(input[1]:size(dim) == input[2]:size(dim), 
+                string.format("input size mismatch, dim %d: %d vs %d", 
+                dim, input[1]:size(dim), input[2]:size(dim)))
+        end
     end
 end
 
--- compute phi(x).
--- according to "Algorithm 2 Tensor Sketch Projection" step 3.
 function CompactBilinearPooling:conv(x,y)
    local batchSize = x:size(1)
    local dim = x:size(2)
@@ -71,76 +62,26 @@ function CompactBilinearPooling:conv(x,y)
    return output:resize(batchSize,1,1,dim,2):select(2,1):select(2,1):select(3,1)
 end
 
-function CompactBilinearPooling:updateOutput(input)
-    -- check two inputs
-    assert(#input == 2, string.format("expect 2 inputs, get %d instead.", #input))
-    -- check 4D inputs
-    for i = 1, #input do
-        assert(input[i]:dim() == 4, string.format("expect 4D input Tensor, get %dD instead for input %d.", input[i]:dim(), i))
-    end
-    -- check shape match for dimension 1, 3, 4
-    for i = 1, input[1]:dim() do
-        if i ~= 2 then
-            assert(input[1]:size(i) == input[2]:size(i), 
-                string.format("dimension %d size mismatch, %d vs %d.", i, input[1]:size(i), input[2]:size(i)))
-        end
-    end
-    self.batchSize = input[1]:size(1)
-    self.height    = input[1]:size(3)
-    self.width     = input[1]:size(4)
-    -- step 1 in algorithm 2.
-    if 0==#self.rand_h_1:size() then
-        self.rand_h_1:resize(input[1]:size(2))
-        self.rand_h_2:resize(input[2]:size(2))
-        self.rand_s_1:resize(input[1]:size(2))
-        self.rand_s_2:resize(input[2]:size(2))
-        self:sample()  -- samples are fixed
-    end
+function ComBiPooling:updateOutput(input)
+    self:checkInput(input)
+    self:genRand(input[1]:size(2), input[2]:size(2))
+    
     -- convert the input from 4D to 2D and expose dimension 2 outside. 
     for i = 1, #input do
-        local input_permute      = input[i]:permute(1,3,4,2):contiguous()
-        self.inputFlatPermute[i] = input_permute:view(-1, input[i]:size(2))
+        local new_input       = input[i]:permute(1,3,4,2):contiguous()
+        self.flat_input[i]    = new_input:view(-1, input[i]:size(2))
     end
-    -- step 2 in algorithm 2.
-    self.flatBatchSize = self.inputFlatPermute[1]:size(1)
-    self.psi:resize(2, self.flatBatchSize, self.outputSize)
-    self:psiFunc()
-    -- step 3 in algorithm 2.
-    self.output_flat = self:conv(self.psi[1], self.psi[2])
+    
+    -- get hash input as step 2
+    self.flat_size = self.flat_input[1]:size(1)
+    self.hash_input:resize(2, self.flat_size, self.output_size)
+    self:getHashInput()
+    
+    -- step 3
+    self.flat_output = self:conv(self.hash_input[1], self.hash_input[2])
     -- reshape output and sum pooling over dimension 2 and 3.
-    self.output       = self.output_flat:reshape(self.batchSize, self.height, self.width, self.outputSize)
+    self.output       = self.flat_output:reshape(input[1]:size(1), input[1]:size(3), input[1]:size(4), self.output_size)
     self.output       = self.output:sum(2):sum(3):squeeze()
     
     return self.output
-end
-
-function CompactBilinearPooling:updateGradInput(input, gradOutput)
-    local batchSize = self.inputFlatPermute[1]:size(1)
-    self.gradInput = self.gradInput or {}
-
-    for k=1, #input do
-        self.gradInputFlatPermute[k] = self.gradInputFlatPermute[k] or self.inputFlatPermute[k].new()
-        self.gradInputFlatPermute[k]:resizeAs(self.inputFlatPermute[k]):zero()
-        self.convBuf = self.convBuf or gradOutput.new()
-        self.convBuf:resizeAs(gradOutput)
-        
-        self.gradOutputRepeat = gradOutput:view(self.batchSize,1,1,self.outputSize):repeatTensor(1,self.height,self.width, 1):view(-1, self.outputSize):contiguous()
-
-        self.convBuf = self:conv(self.gradOutputRepeat, self.psi[k%2+1])
-        if k==1 then
-            self.gradInputFlatPermute[k]:index(self.convBuf, 2, self.rand_h_1)
-            self.gradInputFlatPermute[k]:cmul(self.rand_s_1:repeatTensor(batchSize,1))
-        else
-            self.gradInputFlatPermute[k]:index(self.convBuf, 2, self.rand_h_2)
-            self.gradInputFlatPermute[k]:cmul(self.rand_s_2:repeatTensor(batchSize,1))
-        end
-    end
-   
-   
-    for i = 1, #input do
-        local gradInputView = self.gradInputFlatPermute[i]:view(self.batchSize, self.height, self.width, -1)
-        self.gradInput[i]   = gradInputView:permute(1,4,2,3):contiguous()
-    end
-
-    return self.gradInput
 end

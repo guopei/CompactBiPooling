14cc3a5f5b44d51535780d154300fd8b30548892
Guo Pei <amazingguo@gmail.com>
2017-02-12 01:24:23 -0700

    better way to avoid 1/0 explosion

--- ssr.lua
+++ ssr.lua
@@ -1,3 +1,4 @@
+require 'math'
 local SignedSquareRoot, parent = torch.class('nn.SignedSquareRoot', 'nn.Module')
 
 function SignedSquareRoot:__init(args)
@@ -8,20 +9,23 @@ function SignedSquareRoot:__init(args)
 end
 
 function SignedSquareRoot:updateOutput(input)
-    self.output = self.module:forward(input)
+    self.output = self.output or input.new()
+    self.output_ = self.module:forward(input)
     -- get sign for each input element
     self.sign = self.sign or input.new()
     self.sign:resizeAs(input)
     torch.sign(self.sign, input)
-    self.output:cmul(self.sign)
+    self.output:cmul(self.output_, self.sign)
     
     return self.output
 end
 
 function SignedSquareRoot:updateGradInput(input, gradOutput)
-    --local eps = 1e-4  -- to avoid gradient explosion
-    torch.cmul(self.gradInput, gradOutput, 
-        torch.pow(self.module:forward(input),-1)/2)
+    self.gradInput = self.gradInput or inout.new()
+    self.gradInput:cdiv(gradOutput, 
+        self.output_ * 2)
+    -- filtering out nan, avoid 1/0 caused number explosion
+    self.gradInput[self.output_:eq(0)] = 0
     
     return self.gradInput
 end
